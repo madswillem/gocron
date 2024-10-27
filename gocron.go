00570066dd4d16744a629211e95b367ffc89a905
Mads <121515803+madswillem@users.noreply.github.com>
2024-10-27 17:28:34 +0100

    added tests and removed bugs

--- gocron.go
+++ gocron.go
@@ -7,34 +7,42 @@ import (
 )
 
 type Registry struct {
-	Jobs map[int]Job
+	Jobs map[string]Job
 }
 type Job struct {
-	Job       func(*sync.WaitGroup)
+	Name      string
+	Job       func(*sync.WaitGroup, chan bool, chan error)
 	Ticker    *time.Ticker
 	ErrorChan chan error
+	Done      chan bool
 	Wg        *sync.WaitGroup
 }
 
 func (r *Registry) Add(j Job) error {
-	r.Jobs[len(r.Jobs)] = j
+	if _, ok := r.Jobs[j.Name]; ok {
+		return errors.New("Job already exists")
+	}
+	r.Jobs[j.Name] = j
 	return nil
 }
 
-func (r *Registry) Exec(id int) (*chan error, error) {
-	if entry, ok := r.Jobs[id]; ok {
-		entry.ErrorChan = make(chan error)
+func (r *Registry) Exec(name string) (chan bool, chan error, error) {
+	if entry, ok := r.Jobs[name]; ok {
+		entry.ErrorChan = make(chan error, 1)
+		entry.Done = make(chan bool, 1)
 		entry.Wg = &sync.WaitGroup{}
-		go entry.Job(entry.Wg)
+		entry.Wg.Add(1)
+		go entry.Job(entry.Wg, entry.Done, entry.ErrorChan)
 		go func() {
 			entry.Wg.Wait()
 			close(entry.ErrorChan)
+			close(entry.Done)
 		}()
-		return &entry.ErrorChan, nil
+		return entry.Done, entry.ErrorChan, nil
 	}
-	return nil, errors.New("Job doesnt exist")
+	return nil, nil, errors.New("Job doesnt exist")
 }
 
 func New() *Registry {
-	return &Registry{}
+	return &Registry{Jobs: make(map[string]Job)}
 }
--- gocron_test.go
+++ gocron_test.go
@@ -1,1 +1,100 @@
 package gocron
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestExec(t *testing.T) {
+	data := []struct {
+		name           string
+		job            Job
+		execId         string
+		expectedError  error
+		expectedjoberr error
+		wantPanic      bool
+	}{
+		{
+			name: "test",
+			job: Job{
+				Name: "test",
+				Job: func(wg *sync.WaitGroup, done chan bool, err chan error) {
+					time.Sleep(2 * time.Second)
+					done <- true
+					wg.Done()
+				},
+			},
+			expectedError:  nil,
+			expectedjoberr: nil,
+		},
+		{
+			name: "test_joberr",
+			job: Job{
+				Name: "test_joberr",
+				Job: func(wg *sync.WaitGroup, done chan bool, err chan error) {
+					time.Sleep(2 * time.Second)
+					err <- errors.New("test error")
+					done <- true
+					wg.Done()
+				},
+			},
+			expectedError:  nil,
+			expectedjoberr: errors.New("test error"),
+		},
+		{
+			name: "test_wrong_jobid",
+			job: Job{
+				Name: "test",
+				Job: func(wg *sync.WaitGroup, done chan bool, err chan error) {
+					time.Sleep(2 * time.Second)
+					done <- true
+					wg.Done()
+				},
+			},
+			execId:         fmt.Sprint("test"),
+			expectedError:  errors.New("Job doesnt exist"),
+			expectedjoberr: nil,
+		},
+		{
+			name: "test_jobpanic",
+			job: Job{
+				Name: "test_joberr",
+				Job: func(wg *sync.WaitGroup, done chan bool, err chan error) {
+					time.Sleep(2 * time.Second)
+					panic("test")
+				},
+			},
+			expectedError:  nil,
+			expectedjoberr: nil,
+			wantPanic:      true,
+		},
+	}
+	for _, d := range data {
+		d := d
+		t.Run(d.name, func(t *testing.T) {
+			t.Parallel()
+			defer func() {
+				r := recover()
+				if (r != nil) != d.wantPanic {
+					t.Errorf("SequenceInt() recover = %v, wantPanic = %v", r, d.wantPanic)
+				}
+			}()
+			entry := New()
+			entry.Add(d.job)
+			if d.execId != "" {
+				d.job.Name = d.execId
+			}
+			done, joberr, err := entry.Exec(d.job.Name)
+			<-done
+			if errors.Is(err, d.expectedError) && d.expectedError != nil {
+				t.Errorf("Job error was %e but expected %e", d.expectedError, err)
+			}
+			if e := <-joberr; errors.Is(e, d.expectedjoberr) && d.expectedjoberr != nil {
+				t.Errorf("Job error was %e but expected %e", d.expectedjoberr, e)
+			}
+		})
+	}
+}
